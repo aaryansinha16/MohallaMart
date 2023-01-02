6bc0d1b04886497b156b32e63d28940e84f3124c
aaryansinha16 <aaryansinha16@gmail.com>
2023-01-02 15:35:27 +0530

    integrated websockets for reviews

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -4,5 +4,27 @@ const {Server} = require('socket.io')
 const reviewModel = require("./reviews.model")
 const app = express.Router()
 
+const server = http.createServer(app)
+const io = new Server({
+    cors: {
+        origin: "http://localhost:5173"
+    }
+})
+
+var totalUser = 0
+io.on("connection", (conn) => {
+    totalUser += 1
+    console.log("CONNECT", totalUser)
+
+    conn.on("disconnect", () => {
+        totalUser -= 1
+        console.log("DISCONNECT", totalUser)
+    })
+})
+
+app.get("/", async(req, res) =>{
+    res.send("Working")
+})
+
 
 module.exports = app
